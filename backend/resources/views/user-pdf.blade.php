d61c65270a9dd9ad7f02b71433c0742c5a2594fd
JoanArturo <joanarturo81@gmail.com>
2024-02-13 14:15:30 -0600

    feat: add watermark text

--- backend/resources/views/user-pdf.blade.php
+++ backend/resources/views/user-pdf.blade.php
@@ -5,8 +5,30 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PDF de Usuario</title>
+
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
+
+        #watermark {
+            position: fixed;
+            top: 45%;
+            width: 100%;
+            text-align: center;
+            opacity: .6;
+            transform: rotate(-10deg);
+            transform-origin: 50% 50%;
+            z-index: -1000;
+        }
+    </style>
 </head>
 <body>
+    <div id="watermark">
+        DRAFT
+    </div>
+
+    <h1>Page 1</h1>
     <table border="1">
         <tr>
             <td>Nombre</td>
@@ -21,5 +43,12 @@
             <td>{{ $data['role'] }}</td>
         </tr>
     </table>
+
+    <div class="page-break"></div>
+
+    <h1>Page 2</h1>
+
+    <div class="page-break"></div>
+    <h1>Page 3</h1>
 </body>
 </html>
